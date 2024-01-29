704474cad86da749a5178724720331c519e85695
Liam G <liamg10@yahoo.co.uk>
2024-01-29 16:53:37 +0000

    created main run game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,54 @@
-function runGame(){
+//Run a function after DOM has finished loading
 
-}
-
-function checkAnswer(){
-
-}
-
-function calculateCorrectAnswer(){
-
-}
-
-function incrementScore(){
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
     
+    for (const button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                    alert("You clicked " + gameType + "!")
+            }
+        })
+    }
+});
+
+
+/**
+ * The main loop that runs the game when the DOM is loaded and a users answer has been processed
+ */
+function runGame() {
+    // creates 2 random numbers
+    let num1 = ((Math.random() * 25) + 1) | 0;
+    let num2 = ((Math.random() * 25) + 1) | 0;
 }
 
-function incrementWrongAnswer(){
-    
+function checkAnswer() {
+
 }
 
-function displayAdditionQuestion(){
-    
+function calculateCorrectAnswer() {
+
 }
 
-function displaySubtractQuestion(){
-    
+function incrementScore() {
+
 }
 
-function displayMultiplayQuestion(){
-    
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplayerQuestion() {
+
 }
